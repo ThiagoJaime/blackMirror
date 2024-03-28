cabf5b7cca02bc5a3265eac979a861b868380438
Thiago Santos <106777235+ThiagoJaime@users.noreply.github.com>
2024-03-28 08:49:39 -0300

    Add files via upload

---
+++ black.py
@@ -0,0 +1,16 @@
+question = input("Digite sua pergunta: ").upper()
+
+if question == 'QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO':
+    print("Annie Murphy")
+elif question == 'QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO':
+    print("Computador quântico")
+elif question == 'QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO':
+    print("Streamberry")
+elif question == 'COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA':
+    print("Procurando uma série para assistir")
+elif question == 'QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA':
+    print("procura uma advogada e ouve que não é possível fazer nada contra a Streamberry, já que ela autorizou a companhia a usar suas informações como bem entendesse")
+elif question == "QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR":
+    print("Deep fake e Coleta de dados")
+else:
+    print("Não tem essa pergunta no nosso banco.")
